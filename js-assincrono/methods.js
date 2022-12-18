58e9321d100c32482b4139eeff416ebdc16dac92
herloncosta <herlon36@gmail.com>
2022-12-18 20:34:35 -0300

    Promise.race

--- js-assincrono/methods.js
+++ js-assincrono/methods.js
@@ -1,6 +1,6 @@
 import { wait, rand } from "./promise.js";
 
-// Promise.all
+// Promise.all - retorna array com valores de todas as promises
 const promises = [
   "some value",
   wait("message 1", rand(1, 3)),
@@ -9,5 +9,10 @@ const promises = [
 ];
 
 Promise.all(promises)
-  .then((responses) => console.log(responses))
-  .catch((error) => console.log(error));
+  .then((responses) => console.log(`All: ${responses}`))
+  .catch((error) => console.error(error));
+
+// Promise.race - retorna o valor da primeira promise resolvida
+Promise.race(promises)
+  .then((response) => console.log(`Race: ${response}`))
+  .catch((error) => console.error(error));
