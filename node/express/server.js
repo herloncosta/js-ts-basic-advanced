3aec74f38a6aa56f1c67b3a3dee2a6c876ca0431
herloncosta <herlon36@gmail.com>
2023-01-14 12:19:07 -0300

    add and config mongodb atlas + dotenv

--- node/express/server.js
+++ node/express/server.js
@@ -1,9 +1,21 @@
+import dotenv from 'dotenv'
 import express, { urlencoded } from 'express'
-import { router } from './src/routes.js'
 import { resolve } from 'node:path'
+import { connect, set } from 'mongoose'
+import { router } from './src/routes.js'
 import { example } from './src/middlewares/exampleMiddieware.js'
 
+dotenv.config()
 const app = express()
+
+set('strictQuery', false)
+
+connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log('Connected database.')
+		app.emit('dbconnected')
+	})
+	.catch(e => console.error(e))
 
 app.use(urlencoded({ extended: true }))
 app.use(express.static(resolve('.', 'public')))
@@ -13,4 +25,6 @@ app.use(example)
 app.set('views', resolve('.', 'src', 'views'))
 app.set('view engine', 'ejs')
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.on('dbconnected', () => {
+	app.listen(3000, () => console.log('Server running on port http://localhost:3000'))
+})
